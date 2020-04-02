fde9cea832dbed2793e6dc6351fd8258be593735
ViniciusPuerto <vinicius.alves.porto@gmail.com>
2020-04-02 19:25:45 -0300

    Aligments and some styles for the texts

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Contador De Pessoas",
-    home: Container(color: Colors.white),
+    home: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          "Pessoas: 0",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          "Pode entrar!",
+          style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30),
+        )
+      ],
+    ),
   ));
 }
