feb6a12460d82f2ffc74a364f00906b876203274
ViniciusPuerto <vinicius.alves.porto@gmail.com>
2020-04-02 21:10:29 -0300

    Adding padding on the buttons

--- lib/main.dart
+++ lib/main.dart
@@ -10,21 +10,27 @@ void main() {
           "Pessoas: 0",
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
-        Row(  
+        Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FlatButton(
-                onPressed: () {},
-                child: Text(
-                  "+1",
-                  style: TextStyle(color: Colors.white, fontSize: 40),
-                )),
-            FlatButton(
-                onPressed: () {},
-                child: Text(
-                  "-1",
-                  style: TextStyle(color: Colors.white, fontSize: 40),
-                )),
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: FlatButton(
+                    onPressed: () {},
+                    child: Text(
+                      "+1",
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    )),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: FlatButton(
+                  onPressed: () {},
+                  child: Text(
+                    "-1",
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  )),
+            ),
           ],
         ),
         Text(
