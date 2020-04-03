b9177befc27b21b37aae431a5c629dee4c3e8fb1
ViniciusPuerto <vinicius.alves.porto@gmail.com>
2020-04-02 21:51:34 -0300

    Image as background

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,23 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Contador De Pessoas",
-    home: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          "Pessoas: 0",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
+    home: Stack(children: <Widget>[
+      Image.asset(
+        "images/restaurant.jpg",
+        fit: BoxFit.cover,
+        height: 1000.0,
+      ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Pessoas: 0",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
                 padding: EdgeInsets.all(10.0),
                 child: FlatButton(
                     onPressed: () {},
@@ -21,24 +27,25 @@ void main() {
                       "+1",
                       style: TextStyle(color: Colors.white, fontSize: 40),
                     )),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10.0),
-              child: FlatButton(
-                  onPressed: () {},
-                  child: Text(
-                    "-1",
-                    style: TextStyle(color: Colors.white, fontSize: 40),
-                  )),
-            ),
-          ],
-        ),
-        Text(
-          "Pode entrar!",
-          style: TextStyle(
-              color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30),
-        )
-      ],
-    ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: FlatButton(
+                    onPressed: () {},
+                    child: Text(
+                      "-1",
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    )),
+              ),
+            ],
+          ),
+          Text(
+            "Pode entrar!",
+            style: TextStyle(
+                color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30),
+          )
+        ],
+      ),
+    ],)
   ));
 }
